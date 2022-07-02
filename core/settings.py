a55596a0fb03b1198b2b58a84366a9f53bc1475c
anubhav <anubhavthrilling24@gmail.com>
2022-07-02 11:20:24 +0530

    fix env

--- core/settings.py
+++ core/settings.py
@@ -156,6 +156,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'toodle-backend/static')
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
